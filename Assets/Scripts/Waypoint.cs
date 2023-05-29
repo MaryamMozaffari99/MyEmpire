84ea140847ee2b87df5da72daed86db5693e24c9
Maryam Mozaffari <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-05-29 16:41:38 +0330

    05.29

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -2,3 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Waypoint : MonoBehaviour
+{
+
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] bool isPlaceable;
+
+
+    public bool IsPlaceable { get { return isPlaceable; } }
+
+
+    void OnMouseDown()
+    {
+        if (isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
+    }
+
+}
